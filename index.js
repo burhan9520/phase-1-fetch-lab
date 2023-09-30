c218e79bb3964578b59bd299db684d43e7818325
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2023-09-30 12:47:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Response was not ok');
+    })
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => {
+      console.error('Problem fetching the books:', error);
+    });
 }
 
 function renderBooks(books) {
@@ -12,6 +25,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// Call the fetchBooks function when the HTML is loaded
+document.addEventListener('DOMContentLoaded', fetchBooks);
+
